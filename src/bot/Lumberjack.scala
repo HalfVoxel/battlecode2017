81639c80e1bbcad3fe93f59ecc524fae0c504b8a
Aron Granberg <aron.granberg@gmail.com>
2017-01-12 21:36:41 +0100

    Improve lumberjack

--- src/bot/Lumberjack.scala
+++ src/bot/Lumberjack.scala
@@ -3,6 +3,31 @@ package bot
 import battlecode.common._
 
 class Lumberjack extends Robot {
+
+	private val badTrees = scala.collection.mutable.Map[Int,Int]()
+
+	def findBestTreeToChop() = {
+		val trees = rc.senseNearbyTrees()
+		var bestTree: TreeInfo = null
+		var bestScore = 0f
+		for (tree <- trees) {
+			if (tree.team != rc.getTeam) {
+				val turnsToChopDown = (tree.health/GameConstants.LUMBERJACK_CHOP_DAMAGE) + Math.sqrt(rc.getLocation.distanceTo(tree.location)/info.strideRadius).toFloat + 1f
+				val score = ((if (tree.containedRobot != null) tree.containedRobot.bulletCost * 1.5f else 0) + tree.containedBullets + 1) / turnsToChopDown
+				if (!badTrees.contains(tree.getID) || rc.getRoundNum > badTrees(tree.getID)) {
+					// setIndicatorDot(tree.location, score)
+
+					if (score > bestScore) {
+						bestScore = score
+						bestTree = tree
+					}
+				}
+			}
+		}
+
+		bestTree
+	}
+
 	@throws[GameActionException]
 	override def run() {
 		System.out.println("I'm a lumberjack and I'm okay!")
@@ -11,40 +36,38 @@ class Lumberjack extends Robot {
 		while (true) {
 			// See if there are any enemy robots within striking range (distance 1 from lumberjack's radius)
 			var robots: Array[RobotInfo] = rc.senseNearbyRobots(RobotType.LUMBERJACK.bodyRadius + GameConstants.LUMBERJACK_STRIKE_RADIUS, enemy)
-			val trees = rc.senseNearbyTrees()
-			var closestTree: TreeInfo = null
-			var smallestDistance: Float = 1000;
-			for (tree <- trees) {
-				if (tree.team != rc.getTeam && rc.getLocation.distanceTo(tree.location) < smallestDistance){
-					smallestDistance = rc.getLocation.distanceTo(tree.location)
-					closestTree = tree
-				}
-			}
 			if (robots.length > 0 && !rc.hasAttacked) {
 				// Use strike() to hit all nearby robots!
 				rc.strike()
-			}
-			else {
-				// No close robots, so search for robots within sight radius
-				robots = rc.senseNearbyRobots(-1, enemy)
-				// If there is a robot, move towards it
-				if (closestTree != null && rc.canChop(closestTree.ID)) {
-					rc.chop(closestTree.ID)
-				}
-				if (robots.length > 0) {
+			} else {
+				val bestTree = findBestTreeToChop()
+				if (bestTree != null) {
 					val myLocation: MapLocation = rc.getLocation
-					val enemyLocation: MapLocation = robots(0).getLocation
-					val toEnemy: Direction = myLocation.directionTo(enemyLocation)
-					tryMove(toEnemy)
-				} else if (closestTree != null) {
-					val myLocation: MapLocation = rc.getLocation
-					rc.setIndicatorDot(closestTree.location, 255, 0, 0)
-					val towards: Direction = myLocation.directionTo(closestTree.location)
-					tryMove(towards)
-				}
-				else{
-					// Move Randomly
-					tryMove(randomDirection)
+
+					val towards: Direction = myLocation.directionTo(bestTree.location)
+					if (rc.canChop(bestTree.ID)) {
+						rc.chop(bestTree.ID)
+						rc.setIndicatorLine(rc.getLocation, bestTree.location, 0, 255, 0)
+					} else if (!tryMove(towards)) {
+						// Don't try to chop down this tree until after N turns
+						badTrees(bestTree.getID) = rc.getRoundNum + 25
+						rc.setIndicatorLine(rc.getLocation, bestTree.location, 255, 0, 0)
+					} else {
+						rc.setIndicatorLine(rc.getLocation, bestTree.location, 0, 0, 0)
+					}
+				} else {
+					// No close robots, so search for robots within sight radius
+					robots = rc.senseNearbyRobots(-1, enemy)
+					// If there is a robot, move towards it
+					if (robots.length > 0) {
+						val myLocation: MapLocation = rc.getLocation
+						val enemyLocation: MapLocation = robots(0).getLocation
+						val toEnemy: Direction = myLocation.directionTo(enemyLocation)
+						tryMove(toEnemy)
+					} else {
+						// Move Randomly
+						tryMove(randomDirection)
+					}
 				}
 			}
 			// Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
