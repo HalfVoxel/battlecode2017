51430ff82118d557adec02ff562727792b91faa8
Aron Granberg <aron.granberg@gmail.com>
2017-01-13 02:03:58 +0100

    Make soldiers use the same code as tanks

--- src/bot/Soldier.scala
+++ src/bot/Soldier.scala
@@ -2,27 +2,5 @@ package bot
 
 import battlecode.common._
 
-class Soldier extends Robot {
-	@throws[GameActionException]
-	override def run() {
-		System.out.println("I'm an soldier!")
-		val enemy: Team = rc.getTeam.opponent
-		// The code you want your robot to perform every round should be in this loop
-		while (true) {
-			val myLocation: MapLocation = rc.getLocation
-			// See if there are any nearby enemy robots
-			val robots: Array[RobotInfo] = rc.senseNearbyRobots(-1, enemy)
-			// If there are some...
-			if (robots.length > 0) {
-				// And we have enough bullets, and haven't attacked yet this turn...
-				if (rc.canFireSingleShot) {
-					// ...Then fire a bullet in the direction of the enemy.
-					rc.fireSingleShot(rc.getLocation.directionTo(robots(0).location))
-				}
-			}
-			// Move randomly
-			tryMove(randomDirection)
-			yieldAndDoBackgroundTasks()
-		}
-	}
+class Soldier extends Tank {
 }
