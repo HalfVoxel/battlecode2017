f16d035c1d7ae54fd3bc12bb58ce4b9775c01a40
Mårten Wiman <marten.wiman@gmail.com>
2017-01-16 13:18:29 +0100

    Prefer shooting at enemies with low health

--- src/bot/Scout.java
+++ src/bot/Scout.java
@@ -167,7 +167,8 @@ class Scout extends Robot {
                     }
                     score += 50;
                 }
-                score /= myLocation.distanceTo(robot.getLocation()) + 1;
+                score /= 4 + robot.getHealth() / robot.getType().maxHealth;
+                score /= myLocation.distanceSquaredTo(robot.getLocation()) + 1;
                 if (score > bestScore2) {
                     bestScore2 = score;
                     bestRobot = robot;
